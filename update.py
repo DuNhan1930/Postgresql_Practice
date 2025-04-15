ada334e7ab4fcc1ec057691c15173dfdc75c4259
DuNhan1930 <dunhan1905@gmail.com>
2025-04-15 18:31:16 +0700

    Commit new practice

--- update.py
+++ update.py
@@ -28,5 +28,35 @@ def update_vendor(vendor_id, vendor_name):
     finally:
         return updated_row_count
 
+def update_parts(part_id, part_name):
+    """ Update vendor name based on the vendor id """
+
+    updated_row_count = 0
+
+    sql = """ UPDATE parts
+                SET part_id = %s
+                WHERE part_name = %s"""
+
+    config = load_config()
+
+    try:
+        with  psycopg2.connect(**config) as conn:
+            with  conn.cursor() as cur:
+
+                # execute the UPDATE statement
+                cur.execute(sql, (part_id, part_name))
+                updated_row_count = cur.rowcount
+
+            # commit the changes to the database
+            conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        return updated_row_count
+
 if __name__ == '__main__':
     update_vendor(1, "3M Corp")
+    ls_part_name = ["SIM Tray", "Speaker", "Vibrator", "Antenna", "Home Button", "LTE Modem", "OLED"]
+    for i in range(1, 8):
+        update_parts(i, ls_part_name[i - 1])
+
